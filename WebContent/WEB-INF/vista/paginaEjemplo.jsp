e5a3971bca696d9772c417332fdf257dd33f5b27
nicol <nicol@DESKTOP-FMRJQQ5>
2020-02-19 11:49:51 -0300

    proyecto web realizado con spring con MVC esta parte incluye pagina de prueba y controlador configurado desde webContent.

---
+++ WebContent/WEB-INF/vista/paginaEjemplo.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>IYA LO LOGRE PERRAS!</title>
+</head>
+<body>
+	<h1>Primer ejemplo de mvc con spring</h1>
+</body>
+</html>
---
+++ src/es/pildoras/spring/mvc/Controlador.java
@@ -0,0 +1,14 @@
+package es.pildoras.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Controlador {
+
+	@RequestMapping
+	public String muestraPagina() {
+		
+		return "paginaEjemplo";
+	}
+}
