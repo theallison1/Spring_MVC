1339162611a9ec8a28f48461b1ec8b7d7ca06cc6
nicol <nicol@DESKTOP-FMRJQQ5>
2020-02-20 19:31:15 -0300

    @requestParam  reemplaza el HttpServletRequest para obtener los datos atraves de un identificador

--- src/es/pildoras/spring/mvc/HolaAlumnosControlador.java
+++ src/es/pildoras/spring/mvc/HolaAlumnosControlador.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HolaAlumnosControlador {
@@ -22,9 +23,9 @@ public class HolaAlumnosControlador {
 	}
 
 	@RequestMapping("/procesarFormulario2")
-	public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
-
-		String nombre = request.getParameter("nombreAlumno");
+	//public String otroProcesoFormulario(HttpServletRequest request, Model modelo) {
+	public String otroProcesoFormulario(@RequestParam("nombreAlumno")String nombre, Model modelo) {
+//		String nombre = request.getParameter("nombreAlumno");
 		nombre += " es el mejor alumno";
 
 		String mensajeFinal = "Quien es el mejor alumno?\n" + nombre;
